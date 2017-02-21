de547fa933f3114f39eee989fc117435e8336a6d
rhysgoehring <rhysgoehring@gmail.com>
2017-02-20 18:17:12 -0700

    calc needs error on multiple operators on the screen

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  'use strict';
+
+//  Identify DOM elements -
+  let $clear = $('#clear');
+  console.log("clear is" , $clear)
+  let $screen = $('#screen');
+  console.log("screen is: ", $screen);
+  let $buttons = $('span');
+  console.log("buttons are", $buttons);
+  let $equals = $('#equals');
+  console.log("equals is: ", $equals);
+ 
+
+
+  $('span').each(function(){
+    $(this).click(function(){
+      let $btnVal = $(this).text();
+      console.log("button value is ", $btnVal)
+      let $btnId= $(this).attr('id');
+      if ($btnId === "clear"){
+        $screen.text('');
+      } else if ($btnId === "equals" && $){
+        let $result = eval($screen.text().replace(/x/, '*').replace(/÷/, '/'));
+        $screen.text($result);
+      } else {
+        $screen.text($screen.text() + $btnVal);
+      }
+    })
+  })
+});
